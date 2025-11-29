ca4179674529dc0597214803ea6326a6c7e324b2
zaamaulan <zaamaulan@gmail.com>
2025-11-29 12:54:03 +0700

    commenting tabs trigger fix

--- src/components/team-benefit.tsx
+++ src/components/team-benefit.tsx
@@ -4,7 +4,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Tabs, TabsContent } from '@/components/ui/tabs'
 import { WordRevealText } from './word-reveal-text'
 
 const benefits = [
